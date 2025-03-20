6338996205a66d1d495c844a5ecc048ace28001a
jeevan <masterjeevan88@gmail.com>
2025-03-20 14:19:04 +0530

    Folders created

---
+++ ZestyBeanz/__manifest__.py
@@ -0,0 +1,18 @@
+{
+'name': 'Sample Module',
+'version': '18.0.0.0',
+'summary': 'This Module is for training purposes.',
+'description': """This Module is for training purposes.
+""",
+'category':'',
+'author': ' Jeevan V',
+'website': 'www.zbeanztech.com',
+"license": "LGPL-3",
+'depends': [],
+'data': [],
+'test': [],
+'demo': [],
+'installable': True,
+'auto_install': False,
+'application': False,
+}
