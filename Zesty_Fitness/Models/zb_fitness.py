37429ab8e14663647c680270f898428b230b28a6
jeevan <masterjeevan88@gmail.com>
2025-05-02 10:55:40 +0530

    Assignment update

--- Zesty_Fitness/Models/zb_fitness.py
+++ Zesty_Fitness/Models/zb_fitness.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta
 
 class FitnessMember(models.Model):
     _name = 'fitness.member'
@@ -11,6 +12,20 @@ class FitnessMember(models.Model):
     start_date = fields.Date(string="Start Date")
     end_date = fields.Date(string="End Date")
     active = fields.Boolean(string="Active", default=True)
+    total_amount = fields.Float(string="Total Amount", compute="_compute_total", store=True)
+
+    @api.onchange('start_date', 'membership_plan_id')
+    def _onchange_membership_dates(self):
+        if self.start_date and self.membership_plan_id.duration_days:
+            self.end_date = self.start_date + timedelta(days=self.membership_plan_id.duration_days)
+
+    @api.depends('membership_plan_id.price', 'membership_plan_id.duration_days')
+    def _compute_total(self):
+        for rec in self:
+            if rec.membership_plan_id:
+                rec.total_amount = rec.membership_plan_id.price * rec.membership_plan_id.duration_days
+            else:
+                rec.total_amount = 0.0
 
 
 class FitnessMembership(models.Model):
